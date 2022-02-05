360e7ed971d2a0a10405d85c8b5e988ad086bae5
daviddever <david.dever@cox.net>
2022-02-05 12:57:37 -0800

    added filtering by nick

--- linkshower.py
+++ linkshower.py
@@ -45,3 +45,36 @@ def page(page_id):
                             next_page=next_page,
                             previous_page=previous_page)
 
+@app.route('/<nick>')
+def nick_base_page(nick):
+    return redirect('/{}/1'.format(nick))
+
+@app.route('/<nick>/<int:page_id>')
+def nick_page(nick, page_id):
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+
+    links = []
+
+    limit = str(20)
+    offset = str((page_id * 20) - 20)
+
+    for row in c.execute('''SELECT * FROM links WHERE nick = "{}" ORDER BY rowid
+                            DESC LIMIT {} OFFSET {}'''.format(nick, limit, offset)):
+        links.append(row)
+
+    c.close()
+
+    next_page = page_id + 1
+    if page_id - 1 < 1:
+        previous_page = 1
+    else:
+        previous_page = page_id - 1
+
+    return render_template('links.html',
+                            links=links,
+                            server=server,
+                            channel=channel,
+                            nick=nick,
+                            next_page=next_page,
+                            previous_page=previous_page)
